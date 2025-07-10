5c58a45f6b35928f67478e7e8d003b277f39e7ed
zachmahan <zach.mahan.zm@gmail.com>
2025-07-10 15:55:24 -0400

    working ver

--- projects/bear16_os/os_core.asm
+++ projects/bear16_os/os_core.asm
@@ -23,7 +23,7 @@ month_str_array:
     .string "NOV"
     .string "DEC"
 welcome_msg:
-    .string " Welcome to the Bear16 Console!\n    VERSION 0.0.3, 20250707"
+    .string " Welcome to the Bear16 Console!\n    VERSION 0.0.4, 20250710"
 
 .text
 #WELCOME
