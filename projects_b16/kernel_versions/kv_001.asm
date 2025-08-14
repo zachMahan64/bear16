a4d8ccd7e50af87ed99efe411f2d92b9b174fb6f
zachmahan <zach.mahan.zm@gmail.com>
2025-08-13 22:15:24 -0400

    renamed legacy dir

--- projects_b16/kernel_versions/kv_001.asm
+++
@@ -1,3 +0,0 @@
-# kernel version 0.0.1
-# not used currently
-# the kernelRom feature (rom banking and interrupts) is abandoned in the current os that i wrote, although functionality for it could be (somewhat) easily implemented
