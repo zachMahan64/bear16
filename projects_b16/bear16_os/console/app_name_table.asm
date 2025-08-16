d06206eba0a65685e75960f076131bf640da505e
zachmahan <zach.mahan.zm@gmail.com>
2025-08-15 22:52:19 -0400

    move bear16_os to own repo, rename projects_b16 to examples, add txt file saying to check out the os

--- projects_b16/bear16_os/console/app_name_table.asm
+++
@@ -1,8 +0,0 @@
-# CONSOLE/APP_NAME_TABLE.ASM
-.data
-app_name_table:
-    app_dis_notepad:
-        .string "notepad"
-    app_dis_tic_tac_toe:
-        .string "tictactoe"
-    # add more here!
--- projects_b16/bear16_os/util/constants.asm
+++
@@ -1,6 +0,0 @@
-# CONSTANTS.ASM
-
-.data
-# UNIV CONSTANTS
-.const TRUE = 1
-.const FALSE = 0
